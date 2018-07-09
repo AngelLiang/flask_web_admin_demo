fdb4c34b0a648f2e3b585ed2f41a921e25af32df
阏男秀 <pl01665077@163.com>
2018-07-09 17:21:35 +0800

    update user api

--- app/apis/__init__.py
+++ app/apis/__init__.py
@@ -4,5 +4,4 @@ from flask import Blueprint
 
 api = Blueprint("api", __name__)
 
-from .user_api import init_api as init_user
-init_user(api)
+from .user_api import *
--- app/apis/user_api.py
+++ app/apis/user_api.py
@@ -14,55 +14,59 @@ from app.models import User
 
 from app.exception import TokenTimeOutException
 
+from . import api
 
-class UserAPIObj(object):
 
-    def __init__(self, api=None):
-        self.api = api
 
-    def login(self):
-        values = request.values
-        current_app.logger.debug(values)
+@api.route("user/login", methods=["POST"])
+def login():
+    # 检测参数
+    if not s_check.check_values("username", "password"):
+        return(jsonify(s_json.make_parameter_miss()))
 
-        ret = s_check.check_values("username", "password")
-        if not ret:
-            return(jsonify(s_json.make_parameter_miss()))
-
-        user = User.query.filter_by(username=values.get("username")).first()
-        if user:
-            ret = user.verify_password(values.get("password"))
-            if ret:
-                return jsonify(s_json.make_success(data=user.to_dict()))
-            else:
-                current_app.logger.debug("{} password error!".format(user))
-                return jsonify(s_json.make_fail(message="password error!"))
+    values = request.values
+    current_app.logger.debug(values)
+    
+    # 获取用户
+    user = User.query.filter_by(username=values.get("username")).first()
+    if user:
+        ret = user.verify_password(values.get("password"))
+        if ret: # 成功
+            return jsonify(s_json.make_success(data=user.to_dict()))
         else:
-            current_app.logger.debug("user not found!")
-        return jsonify(s_json.make_fail())
+            current_app.logger.debug("{} password error!".format(user))
+            return jsonify(s_json.make_fail(message="password error!"))
+    else:
+        current_app.logger.debug("user not found!")
 
-    def verify_token(self):
-        ret = s_check.check_values("token")
-        if not ret:
-            return(jsonify(s_json.make_parameter_miss()))
-        token = request.values.get("token")
-        try:
-            data = User.verify_token(token)
-        except TokenTimeOutException:
-            return jsonify(s_json.make_fail(message="token timeout!"))
-        else:
-            user_id = data.get("id")
-            if user_id:
-                user = User.query.get(user_id)
-                if user:
-                    return jsonify(s_json.make_success(data=user.to_dict(has_token=False)))
-        return jsonify(s_json.make_fail())
-
-    def init_api(self, api: Blueprint):
-        self.api = api
-        api.add_url_rule("user/login", view_func=self.login, methods=["POST"])
-        api.add_url_rule("user/token/verify",
-                         view_func=self.verify_token, methods=["POST"])
+    return jsonify(s_json.make_fail())
 
 
-def init_api(api: Blueprint):
-    UserAPIObj().init_api(api)
+
+@api.route("user/logout", methods=["POST"])
+def logout():
+    return jsonify(s_json.make_success())
+
+
+
+@api.route("user/token/verify", methods=["GET", "POST"])
+def verify_token():
+    # 检测参数 
+    if not s_check.check_values("token"):
+        return jsonify(s_json.make_parameter_miss()) 
+    # 获取token
+    token = request.values.get("token")
+
+    # 验证token
+    try:
+        data = User.verify_token(token)
+    except TokenTimeOutException:
+        return jsonify(s_json.make_fail(message="token timeout!"))
+    else:
+        user_id = data.get("id")
+        if user_id:
+            user = User.query.get(user_id)
+            if user:    # 验证成功
+                return jsonify(s_json.make_success(data=user.to_dict(has_token=False)))
+    return jsonify(s_json.make_fail())
+
